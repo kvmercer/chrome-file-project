41c2a3b0bd1e08fccfb32e3c8d4cadf5dddb40c6
Kavin <kv219700>
2021-07-21 16:15:47 -0300

    uneeded function

--- src/background.js
+++ src/background.js
@@ -1,15 +1,3 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [
-				new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: { hostContains: 'youtube'}
-				})
-			],
-			actions: [ new chrome.declarativeContent.ShowPageAction() ]
-		}]);
-	});
-});
 
 chrome.runtime.onMessage.addListener(function(message) {
 	var url = 'http://localhost:4000/download?';
